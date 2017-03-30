3e92eb8df2029af42adbe0c3c3da634e5523e083
Kees Kluskens <kees@webduck.nl>
2017-03-30 18:10:15 +0200

    make it easier to deploy

--- src/View.js
+++ src/View.js
@@ -2,31 +2,31 @@ import { observable, computed } from 'mobx';
 
 const GAMES = [{
     title: 'Plex',
-    poster: '/static/posters/plex.png',
+    poster: 'static/posters/plex.png',
 }, {
     title: 'YouTube',
-    poster: '/static/posters/youtube.png',
+    poster: 'static/posters/youtube.png',
 }, {
     title: 'Battlefield 4',
-    poster: '/static/posters/battlefield-4.jpg',
+    poster: 'static/posters/battlefield-4.jpg',
 }, {
     title: 'Rocket League',
-    poster: '/static/posters/rocket-league.jpg',
+    poster: 'static/posters/rocket-league.jpg',
 }, {
     title: 'Goat Simulator',
-    poster: '/static/posters/goat-simulator.jpg',
+    poster: 'static/posters/goat-simulator.jpg',
 }, {
     title: 'Besiege',
-    poster: '/static/posters/besiege.jpg',
+    poster: 'static/posters/besiege.jpg',
 }, {
     title: 'Grand Theft Auto V',
-    poster: '/static/posters/grand-theft-auto-v.jpg',
+    poster: 'static/posters/grand-theft-auto-v.jpg',
 }, {
     title: 'Battlefield 1',
-    poster: '/static/posters/battlefield-1.jpg',
+    poster: 'static/posters/battlefield-1.jpg',
 }, {
     title: 'Horizon Zero Dawn',
-    poster: '/static/posters/horizon-zero-dawn.png',
+    poster: 'static/posters/horizon-zero-dawn.png',
 }];
 
 export default class ViewStore {
