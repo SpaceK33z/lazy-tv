ce74c0e44224aff2067a18b797c465d3b2c652c7
Kees Kluskens <kees@webduck.nl>
2017-04-01 00:19:14 +0200

    use electron-builder

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -17,7 +17,7 @@ function createWindow() {
 
     const startUrl = process.env.ELECTRON_START_URL ||
         url.format({
-            pathname: path.join(__dirname, '/../build/index.html'),
+            pathname: path.join(__dirname, '/../browser-build/index.html'),
             protocol: 'file:',
             slashes: true,
         });
