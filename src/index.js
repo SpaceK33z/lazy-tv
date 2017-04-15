9ff4fd129f325d1eada4bba75843b8c61d944402
Kees Kluskens <kees@webduck.nl>
2017-04-15 16:03:08 +0200

    Quit app on ctrl+Q
    Fixes #16

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ import 'normalize.css';
 import './styles';
 import App from './container/App';
 import ViewStore from './View';
+import { app } from './electron';
 
 const viewStore = new ViewStore();
+
+window.addEventListener('keydown', (e) => {
+    // Quit app on ctrl+Q
+    if (e.ctrlKey && e.keyCode === 81) {
+        app.quit();
+    }
+});
 
 ReactDOM.render(
     <App store={viewStore} />,
